5f5db734856286d39e428b4c9da2ebe6eb542421
Koro Sensei <69835440+panda1910@users.noreply.github.com>
2020-09-25 19:39:01 +0530

    Add files via upload

---
+++ P1-10072020.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+//Hello World Program
+
+int main()
+{
+    cout << "Welcome to the world of C++" << endl;
+    return 0;
+}
---
+++ P2-10072020.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+//Print out basic details
+
+int main()
+{
+    cout << "Name: Aviral Rana" <<endl;
+    cout << "Course: B. Tech" <<endl;
+    cout << "Semester: III" <<endl;
+    cout << "Section: CST" <<endl;
+    cout << "Roll no: 14" <<endl;
+    return 0;
+}
